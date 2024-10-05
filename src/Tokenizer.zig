68db773fbe72506b6effdc45adfd72ba9802b4ab
vidar-h <electsmail@gmail.com>
2024-10-05 23:06:02 +0200

    Start of the tokenizer

---
+++ src/Tokenizer.zig
@@ -0,0 +1,142 @@
+const std = @import("std");
+const Tokenizer = @This();
+
+const TokenType = enum(u8) {
+    @"+",
+    @"-",
+    @"*",
+    @"/",
+    @"if",
+    @"else",
+    @"=",
+    @";",
+    @"(",
+    @")",
+    int_literal,
+    invalid,
+    identifier,
+};
+
+const Token = struct {
+    type: TokenType,
+    start: u32,
+    end: u32,
+
+    const keywords = std.StaticStringMap(TokenType).initComptime(.{
+        .{ "if", .@"if" },
+        .{ "else", .@"else" },
+    });
+};
+
+// Only having one enum value in this will crash the compiler ¯\_(ツ)_/¯
+const State = enum {
+    start,
+    identifier,
+    int_literal,
+};
+
+src: [:0]const u8,
+index: u32,
+
+pub fn init(src: [:0]const u8) Tokenizer {
+    return .{ .src = src, .index = 0 };
+}
+
+pub fn next(self: *Tokenizer) ?Token {
+    var result: Token = .{
+        .type = undefined,
+        .start = self.index,
+        .end = undefined,
+    };
+
+    state: switch (State.start) {
+        .start => switch (self.src[self.index]) {
+            '\n', '\t', ' ', '\r' => {
+                self.index += 1;
+                result.start = self.index;
+                continue :state .start;
+            },
+            // Could combine these if we commit to having a fixed enum order.
+            '+' => {
+                self.index += 1;
+                result.type = .@"+";
+            },
+            '-' => {
+                self.index += 1;
+                result.type = .@"-";
+            },
+            '*' => {
+                self.index += 1;
+                result.type = .@"*";
+            },
+            '/' => {
+                self.index += 1;
+                result.type = .@"/";
+            },
+            '=' => {
+                self.index += 1;
+                result.type = .@"=";
+            },
+            ';' => {
+                self.index += 1;
+                result.type = .@";";
+            },
+            '(' => {
+                self.index += 1;
+                result.type = .@"(";
+            },
+            ')' => {
+                self.index += 1;
+                result.type = .@")";
+            },
+            'a'...'z', 'A'...'Z', '_' => {
+                result.type = .identifier;
+                continue :state .identifier;
+            },
+            '0'...'9' => {
+                result.type = .int_literal;
+                continue :state .int_literal;
+            },
+            0 => {
+                if (self.index == self.src.len) {
+                    return null;
+                }
+
+                result.type = .invalid;
+                self.index += 1;
+            },
+            else => {
+                result.type = .invalid;
+                self.index += 1;
+            },
+        },
+        .identifier => {
+            self.index += 1;
+            switch (self.src[self.index]) {
+                // Still going
+                'a'...'z', 'A'...'Z', '_', '0'...'9' => continue :state .identifier,
+
+                // Hit end.
+                else => {
+                    const identifier_string = self.src[result.start..self.index];
+                    if (Token.keywords.get(identifier_string)) |t| {
+                        result.type = t;
+                    }
+                },
+            }
+        },
+        .int_literal => {
+            self.index += 1;
+            switch (self.src[self.index]) {
+                '0'...'9' => {
+                    self.index += 1;
+                    continue :state .int_literal;
+                },
+                else => {},
+            }
+        },
+    }
+
+    result.end = self.index;
+    return result;
+}
---
+++ src/main.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const Allocator = std.mem.Allocator;
+const Tokenizer = @import("Tokenizer.zig");
+
+pub fn main() !void {
+    var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
+    defer assert(gpa_state.deinit() == .ok);
+
+    const gpa = gpa_state.allocator();
+
+    const args = try std.process.argsAlloc(gpa);
+    defer std.process.argsFree(gpa, args);
+
+    if (args.len != 2) {
+        // No error reporting for now.
+        return error.InvalidArgs;
+    }
+
+    const source_code = try std.fs.cwd().readFileAllocOptions(gpa, args[1], 1_000_000_000, null, std.mem.page_size, 0);
+
+    defer gpa.free(source_code);
+
+    var tokenizer = Tokenizer.init(source_code);
+    while (tokenizer.next()) |token| {
+        std.debug.print("{any}\n", .{token});
+    }
+}
